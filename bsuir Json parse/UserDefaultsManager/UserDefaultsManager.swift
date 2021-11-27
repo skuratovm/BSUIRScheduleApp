17147b6af546bfe68531d998c795b601ce883b5b
Mikhail <maydibee@gmail.com>
2021-11-27 21:02:50 +0300

    feat: add uploading data from user defaults

--- bsuir Json parse/UserDefaultsManager/UserDefaultsManager.swift
+++ bsuir Json parse/UserDefaultsManager/UserDefaultsManager.swift
@@ -1,38 +1,36 @@
-////
-////  UserDefaultsManager.swift
-////  bsuir Json parse
-////
-////  Created by Mikhail Skuratov on 26.11.21.
-////
 //
-//import Foundation
+//  UserDefaultsManager.swift
+//  bsuir Json parse
 //
-//class DataBase{
-//    static let shared = DataBase()
-//    enum settingsKeys: String{
-//        case card
-//    }
-//    let cardKey = settingsKeys.card.rawValue
+//  Created by Mikhail Skuratov on 26.11.21.
 //
-//    let defaults = UserDefaults.standard
-//
-//    var cards:[UserCardData] {
-//        get {
-//            if let data = defaults.value(forKey: cardKey) as? Data{
-//                return try! PropertyListDecoder().decode([UserCardData].self, from: data)
-//            } else {
-//                return [UserCardData]()
-//            }
-//        }
-//        set {
-//            if let data = try? PropertyListEncoder().encode(newValue){
-//                defaults.set(data,forKey: cardKey)
-//            }
-//        }
-//    }
-//    func saveCard(userName: String,cardNumber: String, cardExperationDate: String){
-//       let card = UserCardData(userName: userName, cardNumber: cardNumber, cardExperationDate: cardExperationDate)
-//        cards.insert(card, at: 0)
-//    }
-//}
-//
+
+import Foundation
+
+class DataBase{
+    static let shared = DataBase()
+   
+    //let cardKey = settingsKeys.card.rawValue
+
+    let defaults = UserDefaults.standard
+
+    var schedules:[ScheduleModel]? {
+        get {
+            if let data = defaults.value(forKey: "data") as? Data{
+                return try! PropertyListDecoder().decode([ScheduleModel].self, from: data)
+            } else {
+                return [ScheduleModel]()
+            }
+        }
+        set {
+            if let data = try? PropertyListEncoder().encode(newValue){
+                defaults.set(data,forKey: "data")
+            }
+        }
+    }
+    func saveSchedule(result: ScheduleModel?){
+        
+        schedules?.insert(result!, at: 0)
+    }
+}
+
--- bsuir Json parse/ViewController.swift
+++ bsuir Json parse/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     var result: ScheduleModel?
+    
+    var resultMemory = DataBase.shared.schedules?[0]
+    
 
     @IBOutlet weak var tableView: UITableView!
     let GroupSection = ["Понедельник","Вторник","Среда", "Четверг","Пятница","Суббота"]
-    var schedules = [ExamScheduleElement]()
+   // var schedules = [ExamScheduleElement]()
 //    var subjs = [TodayScheduleElement]()
     
     
@@ -24,6 +27,10 @@ class ViewController: UIViewController {
             if error == nil{
                 guard let scheduleModel = scheduleModel else {return}
                 self?.result = scheduleModel
+                //MARK:save data to UseDefaults
+                
+                DataBase.shared.saveSchedule(result: self?.result)
+                ///
                 print(self?.result)
                 self?.tableView.reloadData()
             } else {
@@ -31,6 +38,7 @@ class ViewController: UIViewController {
             }
         }
         
+      
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +47,10 @@ class ViewController: UIViewController {
         tableView.register(UINib(nibName: "CellTableViewCell", bundle: nil), forCellReuseIdentifier: "InfoCell")
         // Do any additional setup after loading the view.
         fetchSchedule(groupNumber: "920605")
+        if resultMemory != nil{
+            tableView.reloadData()
+        }
+        print("🥰:\(resultMemory):😘")
     }
 
 
@@ -46,11 +58,13 @@ class ViewController: UIViewController {
 
 extension ViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let result = result{
+        if let result = resultMemory{
             return result.schedules[section].schedule.count
+            
         }
         return 0
     }
+    
     
    
     
@@ -58,26 +72,44 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
   
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return result?.schedules.count ?? 0
+        return resultMemory?.schedules.count ?? 0
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
         let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell", for: indexPath) as! CellTableViewCell
-        let subjtext = (result?.schedules[indexPath.section].schedule[indexPath.row].subject)!
-        let sttimetxt = (result?.schedules[indexPath.section].schedule[indexPath.row].startLessonTime)!
-        let endTimeTxt = (result?.schedules[indexPath.section].schedule[indexPath.row].endLessonTime)!
-        let lessonTypeTxt = (result?.schedules[indexPath.section].schedule[indexPath.row].lessonType.rawValue)!
+     
+        
+        var subjtext = (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].subject)
+        if subjtext == nil{
+            subjtext = (result?.schedules[indexPath.section].schedule[indexPath.row].subject)!
+        }
+           
+        
+        
+        
+        
+      //  let subjtext = (result?.schedules[indexPath.section].schedule[indexPath.row].subject)!
+        
+        
+        
+        
+        
+       ///////
+        let sttimetxt = (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].startLessonTime)!
+        let endTimeTxt = (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].endLessonTime)!
+        let lessonTypeTxt = (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].lessonType.rawValue)!
        var teacherTxt = ""
-        if result?.schedules[indexPath.section].schedule[indexPath.row].employee.indices.contains(0) == true {
-            teacherTxt =  (result?.schedules[indexPath.section].schedule[indexPath.row].employee[0].fio)!
+        if resultMemory?.schedules[indexPath.section].schedule[indexPath.row].employee.indices.contains(0) == true {
+            teacherTxt =  (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].employee[0].fio)!
             
         }
         var classRoomTxt = ""
 //        classRoomTxt = (result?.schedules[indexPath.section].schedule[indexPath.row].auditory[0])!
         
-        if result?.schedules[indexPath.section].schedule[indexPath.row].auditory.indices.contains(0) == true{
-            classRoomTxt = (result?.schedules[indexPath.section].schedule[indexPath.row].auditory[0])!
+        if resultMemory?.schedules[indexPath.section].schedule[indexPath.row].auditory.indices.contains(0) == true{
+            classRoomTxt = (resultMemory?.schedules[indexPath.section].schedule[indexPath.row].auditory[0])!
         }
         
         
@@ -87,7 +119,9 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
         //let weekDayTxt = (result?.schedules[indexPath.section].weekDay)!
         
         print("😠\(subjtext)")
-        cell.configureCell(subjtext: subjtext, sttimetxt: sttimetxt, endtimetxt: endTimeTxt, classRoomTxt: classRoomTxt, teacherTxt: teacherTxt, lessonType: lessonTypeTxt )
+        cell.configureCell(subjtext: subjtext!, sttimetxt: sttimetxt, endtimetxt: endTimeTxt, classRoomTxt: classRoomTxt, teacherTxt: teacherTxt, lessonType: lessonTypeTxt )
+       
+       
         return cell
     }
     
@@ -96,7 +130,7 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return result?.schedules[section].weekDay
+        return resultMemory?.schedules[section].weekDay
         
     }
     
@@ -106,6 +140,7 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
         present(vc, animated: true,completion: nil)
     }
     
+   
 
     
     
